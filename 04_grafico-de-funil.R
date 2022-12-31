85a5baecf294c57c4d4b978397c276e1d2c0e9d3
Alleh Nogueira <nogueira.aksn@gmail.com>
2022-12-31 18:50:40 -0300

    Add codes for funnel plots

---
+++ 04_grafico-de-funil.R
@@ -0,0 +1,44 @@
+options(digits = 2)
+
+if (!require("pacman")) install.packages("pacman")
+p_load(tidyverse, cowplot, readxl, meta)
+
+sd_from_ci <- function(lower_ci_limit, upper_ci_limit, sample_size){
+    round(sqrt(sample_size) * (upper_ci_limit - lower_ci_limit) / 3.92, 2)
+}
+
+auroc <- read_excel("data-auroc.xlsx") |>
+mutate(
+    study = paste0(study, " (", publication_year, ")"),
+    auroc_grace = grace_auc,
+    sd_auroc_grace = sd_from_ci(grace_lower_ci, grace_upper_ci, n),
+    auroc_grace_glucose = grace_glucose_auc,
+    sd_auroc_grace_glucose = sd_from_ci(
+        grace_glucose_lower_ci, grace_glucose_upper_ci, n
+    )
+)
+
+meta_model <- metacont(
+    data = auroc,
+    studlab = study,
+    n.e = n, n.c = n,
+    mean.e = auroc_grace_glucose, mean.c = auroc_grace,
+    sd.e = sd_auroc_grace_glucose, sd.c = sd_auroc_grace
+)
+
+pdf("figura-s1.pdf", width = 10, height = 10)
+funnel(
+    meta_model,
+    xlab = expression(bold("Diferença Média na ASCROC")),
+    ylab = expression(bold("Erro Padrão")),
+    contour = c(0.95, 0.975, 0.99),
+    col.contour = c("#3B2E4D", "#6C548C", "#9C89B8"),
+    alpha = 0.6
+)
+legend(
+    x = -0.10, y = 0.08,
+    c("P < 0.05", "P < 0.025", "P < 0.01"),
+    bty = "n",
+    fill = c("#3B2E4D", "#6C548C", "#9C89B8")
+)
+dev.off()
